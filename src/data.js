5cd9fd65e32e4b6df42b6960d85df62c4babe525
maansik2000 <maansik2000@gmail.com>
2021-04-13 15:35:12 +0530

    changed

--- src/data.js
+++ src/data.js
@@ -6,7 +6,7 @@ function music() {
             name : "colors",
             cover : "https://images.genius.com/2ecb15f79c7d5e0f4c6d1cec9e4ea3c7.543x543x1.jpg",
             artist : "Halsey",
-            audio : "https://media.hungama.com/c/4/47e/140/41478706/41478706_128.mp3?sA5I7VmYiRhk7_rPw6dH9FU43zXg-etPnCyn_Nc4e2EDwzK9mXAV6AlN8vLtGM5RZuehjyLqU3OWwyZy2XxX1QVS1eyGbOc_F4W6HMD_mRTyGnstxzKoNC1Oi7JEs8iAyTv2qg",
+            audio : "https://media.hungama.com/c/4/333/ed7/56602579/56602579_128.mp3?wpSaKsK-c_pPQ1gNVd2VCqkbQCcQOKSApU7KK80PYqTInE_ezNDNdzXLiKMGxSN-mRC0iKjdTlgiy92784SDj3MdOZ56IWt1ENm-enj7orE4T7kq7djsuJ2MtgZLtLVKZWN4PA",
             color : ["#597B87","#CA9EA6"],
             id : uuidv4(),
             active : true
@@ -15,7 +15,7 @@ function music() {
             name : "Intention",
             cover : "https://f4.bcbits.com/img/a4282832058_10.jpg",
             artist : "Justin Bieber",
-            audio : "https://media.hungama.com/c/4/ec0/a02/51733776/51733776_128.mp3?g17VGsQC0vseaaIH49nnhqymP-iz_GRD9Uvx3UKdvzeBxfBXn3_UWB1lKXkWDPpUQ_MqQZ_ZbQrn7lC86IyIeYiRrpc6GWbkjaVC5Fk73xR1Vsxb28NdVj8eOCCxE-FWHU3wdg",
+            audio : "https://media.hungama.com/c/4/ec0/a02/51733776/51733776_128.mp3?O8UuHF539mA2EZfv7PmR-MxuqW78azphDPD7-I6Wa_An3S25XXcuBGTK86P2imbVelx0f2lu-JafhNKwTLRMZmhYUkrlNWJmyBCjysmonqqfKDDq1wYkPirVsBLb23HC6zwGOA",
             color : ["#CE3F33","#93282D"],
             id : uuidv4(),
             active : false
@@ -24,7 +24,7 @@ function music() {
             name : "Be kind",
             cover : "https://www.lololyrics.com/img/cover/39824.jpeg",
             artist : "Marshmello, Hasley",
-            audio : "https://media.hungama.com/c/4/2fb/1ce/58185306/58185306_128.mp3?tPoZwud7KUfNnfBkog5i9sipwEdRKPRWQk8D9tlSFRT8Q5oPV-vcYqteLd42WsFt64vQVwwrv-uch48dMI6asZbd1qwyv3qNmJLLEFQcY0TAQZ7OkKisCpdSC4CNMi2Q1ilrWw",
+            audio : "https://media.hungama.com/c/4/2fb/1ce/58185306/58185306_128.mp3?8dG50JXafEYL02LsuFXohd4kBRi7NthkNdfkZH5-BCtQMAa7z10-GbJST8vaXs2dBwWjL4YAGORMJNPpvtx1KIsTmUcpnIVtvnTYqqbVQ90KqiCpjRWgVdersWzRGGvJgJ6KYQ",
             color : ["#F1727B","#C7DEE2"],
             id : uuidv4(),
             active : false
@@ -33,7 +33,7 @@ function music() {
             name : "Wrong Direction",
             cover : "https://m.media-amazon.com/images/I/81DgGIc6ijL._SS500_.jpg",
             artist : "Hailee Steinfeld",
-            audio : "https://media.hungama.com/c/4/2a4/c5e/53505707/53505707_128.mp3?UNuUqNi97LM48uqsP3_2hxy0zThbb2vpSAGxpBF3yH4B4_kNP4oAkBOl7_Q2DkRcijS9hNmgYT38F3cC27jqmRzdU5vGokzMe-X-xOe7-1ytRTSz9Olp3f3SZwKD-wA5ktOlZA",
+            audio : "https://media.hungama.com/c/4/2a4/c5e/53505707/53505707_128.mp3?OKnljd7eOMVQTLlQ00peI2bwL-x1pFoOEE470Mxz6pdS1f3C205ECx6w5adrAT8Lg1KFh7JyVswV0ap-skqHInX8nmgA4G5FZF0oo9wDkO5bNdbg4e-HNdmdf2hwEAK0xl_BBw",
             color : ["#C63E64","#FFDDEA"],
             id : uuidv4(),
             active : false
@@ -42,7 +42,7 @@ function music() {
             name : "Always i'll care",
             cover : "https://images.genius.com/d38bc193eaa95ac28da1507867856d7b.1000x1000x1.jpg",
             artist : "Jeremy Zucker",
-            audio : "https://media.hungama.com/c/4/59f/819/53142693/53142693_128.mp3?_5U6ZJpoyDYmVe_T2ZrPx0FJxJkrSWlU6ZrHtRP_ovsRARcJPP-t_0T9ZlNYlYsZzghWnPuSDTrKx4xVGFmwtfzv0G9kG0vUHgjcz4FWABUDqt8xQjAn_mPl6oQrYoxF0jDtKw",
+            audio : "https://media.hungama.com/c/4/59f/819/53142693/53142693_128.mp3?E8HiTqimAP-75fqvqRTqLr3NI7eYp_gONvWxsBL9nmhPO91JYEN5PtVv24o6LiEwfq26xYqcTgfCWYaiAwNcrAfP8vermYDJ11MrHuWCVH79MXxyGW3kWGeIHZXlLNwZofhNUA",
             color : ["#4E5068","#CC6F56"],
             id : uuidv4(),
             active : false
@@ -51,17 +51,8 @@ function music() {
             name : "All you need to know",
             cover : "https://images.hungama.com/c/1/e3b/ffe/61120983/61120983_300x300.jpg",
             artist : "Gryffin",
-            audio : "https://media.hungama.com/c/4/3ae/75d/61120989/61120989_128.mp3?UQc8XUh8BGRAb3T22VJooYnT-stfpvzVV3NbGwR7S33vJouuuDQmwpClBBhyaeEJJ43yu19miK2E_p8N3d-WIYgP4RXap9m45n5UQnwtC6QL_JIO1ywqe-Qz1uX7dEaTEo-39w",
+            audio : "https://media.hungama.com/c/4/63b/c48/61120843/61120843_128.mp3?2DWxMSIrXD_B2aqAzZdvHE71mkxf-qO5ZKV8B2Vb2BebeE8ufWagXIrRhpPYWKfgajqG1xgc5z2se1az0ggEIHjEUjXLq_hC2LnN96QnnkT2jhr_Wg3IzxXQ13Kb9tUI2B2mqQ",
             color : ["#BCE5F9","#6E7F8F"],
-            id : uuidv4(),
-            active : false
-        },
-        {
-            name : "2U",
-            cover : "https://m.media-amazon.com/images/I/71G8-6kNgEL._SS500_.jpg",
-            artist : "Justin Bieber",
-            audio : "https://media.hungama.com/c/4/c67/f96/36944606/36944606_128.mp3?ERG9rfmRRS3HRQeZb2SH6n9DpSLOqeMK-AhzOpPVtXNC0JrtdPsVu6K3CcI0jE9mEYUFrK-XVykPjkX9PiMjw-b_hBP8qHHyw1CrBgDo3kJlDvDvVKEF_u707fhIFhqrs3r1eQ",
-            color : ["#6517C3","#1990E0"],
             id : uuidv4(),
             active : false
         },
@@ -69,7 +60,7 @@ function music() {
             name : "Stacy",
             cover : "https://images.genius.com/353670ca2f209fa9670dd08cf4f06151.1000x1000x1.jpg",
             artist : "Quinn XCII",
-            audio : "https://media.hungama.com/c/4/9cd/47d/48278447/48278447_128.mp3?lLszceVQWgSrAhAnXD-euewMz9rMtZQ4wwoFughAtYGhXGtcMXWQRMCCzprFQXCh-8AinlL1bKwIECK5OzYuBbtA-1nlj9n8xYSDCmJbe8dWZAUdrgeEHBROgjV0ttrS9Zg-tQ",
+            audio : "https://media.hungama.com/c/4/d7f/894/49025100/49025100_128.mp3?rgQ9Y4o6zqUZyRBDCFA1W4BfEEsyhBqI19SdtGBqc4ciKsRQpmIDeeUvK4I1leK9KZHbgFEwSFmHFnVuEkb_0Pfvb2dYCrA-1nAglS_YD7jIvr3o1-WetGVlopEkqo-urNSlCA",
             color : ["#63690E","#E9E8ED"],
             id : uuidv4(),
             active : false
@@ -78,16 +69,7 @@ function music() {
             name : "This is Us",
             cover : "https://abokiplay.com/wp-content/uploads/2020/02/Tis-Is-us.jpg",
             artist : "Jimmi allen, Noah Cyrus",
-            audio : "https://media.hungama.com/c/4/6f8/816/51955590/51955590_128.mp3?6Rw1qNcPSpQLNIRM7OlZcAAVQb_uYILhCR86zEOJatEDBRbzcLoTEBgpvvh8vS7lmnB-iOlagLaS8XGNbu8JBzIOoYpo1wzIjn6mnqcD1HFrtWTH6bagjlmRzryQtI1L3OjEIw",
-            color : ["#545454","#E9E8ED"],
-            id : uuidv4(),
-            active : false
-        },
-        {
-            name : "Mirchi",
-            cover : "https://images.hungama.com/c/1/d8d/2e2/58977937/58977937_300x300.jpg",
-            artist : "Divine",
-            audio : "https://media.hungama.com/c/4/5af/25f/58977938/58977938_128.mp3?xo19TkMGy4ZCvsIUcFE5C_Z7qC1bd7wLb1dFzxu7UtvFistCaxkkr0-KaTsN9ZNU5oqMiQ_mcyUM_jR7jN4gOJ7vm9hT1kKUIFSKPg5Zf4O7oRfbacOKYDaO4wCK8BXrk8ukWw",
+            audio : "https://media.hungama.com/c/4/6f8/816/51955590/51955590_128.mp3?YX08vHhMHnYd_j6MC2h80HTNJoQrz14L0PQxwLhqdbP0SiqU_hgm0fqVl51_KwCmo3UVDVs4EL6e8nZXnM52OksXPJcDRv9tTpUCeQXTy97xeKnIhRkbxX_ElDheBbQbQHCVFg",
             color : ["#545454","#E9E8ED"],
             id : uuidv4(),
             active : false
@@ -96,25 +78,16 @@ function music() {
             name : "What A Time",
             cover : "https://www.rollingstone.com/wp-content/uploads/2019/02/julia_michaels-main_press_photo-_pc_clare_gillenW.jpg",
             artist : "Julia Michaels",
-            audio : "https://media.hungama.com/c/4/6f5/358/46473866/46473866_128.mp3?OdMk1sLGBfTSI8FaXo8ey8hhFYKAfOhmALdRWCKyo24nGkttzjPDW2GABSem4o8CT2b_4qXAkl8UAtldPCIkmz56JnoQoJRSm-KPZHv2ZFTovAFmMJoN1ZcVC2Xi0p5SAtliBg",
+            audio : "https://media.hungama.com/c/4/6f5/358/46473866/46473866_128.mp3?cwAb8n38RCopD_FwYxDo9q7Y_Hqxv5ADGXHVuFcrq6cW9HefgU-0qM9d32AtzjIZMT2ABqnxRrtF5NJBUQTCoWbvjqzBnD8NoR-VrU9OFTd3zM-0Z6whP4JVlHNEous4vPmsMQ",
             color : ["#C39884","#FFFFFF"],
             id : uuidv4(),
             active : false
         },
         {
             name : "Slow",
-            cover : "https://images.hungama.com/c/1/811/fd5/54594369/54594369_300x300.jpg",
-            artist : "Shy Martin",
-            audio : "https://media.hungama.com/c/4/f92/a34/54594370/54594370_128.mp3?BWQgCKFCQ4JrkAr_6G_Kg25SkfsTuxuPHqDLzOAMLco9HrjqgbLcmCKLXMlKr-Su6IE5Mx43Jq9uQvDnpbHqpKdalNmBc_7xTHN7AHFymNYhQ6xfdkkJUYnGuGFN1jrQfSrOBg",
-            color : ["#31485B","#D4CDA5"],
-            id : uuidv4(),
-            active : false
-        },
-        {
-            name : "Night Sky",
             cover : "https://images.hungama.com/c/1/262/45b/55760515/55760515_300x300.jpg",
             artist : "Shy Martin",
-            audio : "https://media.hungama.com/c/4/24d/f07/55760517/55760517_128.mp3?oQXBWyZYaZDofkmLmL7AgJymMnVRrLxk3kQtj5iNSBKJPwRXGokA37vnFeHlWsxcI6Cb8-L7E1CcfE3O0dawamfgUVsteD0lTh4AnL1E_rzhKI6mwLtRrecGo2xSw7vKlkgvwA",
+            audio : "https://media.hungama.com/c/4/f92/a34/54594370/54594370_128.mp3?d8GHbEokT-sUJefUggiNVcP3O75eDsAZQq1_Vzxn5u-yB8tpZqh-v09KsCdmVtBAgkYt8MU8o-EBLGx8SzEdfSa4WfmxryCXXjTL74_8aP4J2-vSaRSXyFuCKgkd1fFwjoVY5w",
             color : ["#532C45","#B9605C"],
             id : uuidv4(),
             active : false
@@ -123,7 +96,7 @@ function music() {
             name : "Twenty One",
             cover : "https://images.hungama.com/c/1/a5d/5c6/47560946/47560946_300x300.jpg",
             artist : "Khalid",
-            audio : "https://media.hungama.com/c/4/644/bf6/47561020/47561020_128.mp3?9lf86YNtBuuTxf5wSynoT_qI6g4FyuD9u2L0mNCgjG8Dx8SmNbesO7mEJnHtZpwso9dn5_neYY-YyGqjGnbZvbHTgbe7zZCostiJhWrQpD9PjQr-RFJoj4uBrzN8zUeBsWtg2w",
+            audio : "https://media.hungama.com/c/4/644/bf6/47561020/47561020_128.mp3?iGUvoJ3y9xZDReyhsH6ZJLlYN7jKbDR6sdJzPVp2zdIAJOQnRHXL93XmcNVvq6jZUQ92KyCMNVt9TYGkRjyh3Gf8NjokX-mKnRBfLub6FEuDBCSfZ3gbdIZcJ1kCVPhW8aJPtw",
             color : ["#956222","#85B3CE"],
             id : uuidv4(),
             active : false
@@ -132,7 +105,16 @@ function music() {
             name : "Free Spirit",
             cover : "https://images.hungama.com/c/1/a5d/5c6/47560946/47560946_300x300.jpg",
             artist : "Khalid",
-            audio : "https://media.hungama.com/c/4/5f2/ab6/47561018/47561018_128.mp3?10VE5WZ4llh3fvY-IQNPtjFhzguDGodLihm18NBMRmK9RdePISykK8ZuuHupYhovrtWbbQzjac0cU5IT9uHRdCSt0hdFr2lD2_4Vcltmr7OnuiS14TzSnpE0-ceoWe4EvoYJUw",
+            audio : "https://media.hungama.com/c/4/5f2/ab6/47561018/47561018_128.mp3?Gz3m9I5ZocmZhmMVnB1_Q7VKe6ToDlF2McWRAHzBWg27TSoI6yMbF_g5-dWbiVpBG_mivuNuJeCro_h7TSOoURlTmGP0pMrm8OBU5wAlBH9b25-ozMZHX-1xEQZzxPDDQLbqyA",
+            color : ["#956222","#85B3CE"],
+            id : uuidv4(),
+            active : false
+        },
+        {
+            name : "Saturday Night",
+            cover : "https://images.hungama.com/c/1/a5d/5c6/47560946/47560946_300x300.jpg",
+            artist : "Khalid",
+            audio : "https://media.hungama.com/c/4/db1/198/47561033/47561033_128.mp3?Q5rkR0NiR5cwl--yau-m3tmxodmOzG7QB8PXQcC16UYsavOSfVK_52ybQMXdg4F03vgwNPKeiWm7R5ctqYF_QAR15poN2IBJaq3gNLhd24BeSo9HYaOk-VFA4FMr8K5AIXVNyA",
             color : ["#956222","#85B3CE"],
             id : uuidv4(),
             active : false
