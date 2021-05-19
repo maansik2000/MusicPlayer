8a6bf14ddd61545dde598516834c67abfef5b66e
maansik2000 <maansik2000@gmail.com>
2021-05-19 15:13:54 +0530

    changed

--- src/data.js
+++ src/data.js
@@ -3,92 +3,11 @@ import { v4 as uuidv4 } from "uuid";
 function music() {
     return [
         {
-            name : "colors",
-            cover : "https://images.genius.com/2ecb15f79c7d5e0f4c6d1cec9e4ea3c7.543x543x1.jpg",
-            artist : "Halsey",
-            audio : "https://media.hungama.com/c/4/333/ed7/56602579/56602579_128.mp3?wpSaKsK-c_pPQ1gNVd2VCqkbQCcQOKSApU7KK80PYqTInE_ezNDNdzXLiKMGxSN-mRC0iKjdTlgiy92784SDj3MdOZ56IWt1ENm-enj7orE4T7kq7djsuJ2MtgZLtLVKZWN4PA",
-            color : ["#597B87","#CA9EA6"],
-            id : uuidv4(),
-            active : true
-        },
-        {
-            name : "Intention",
-            cover : "https://f4.bcbits.com/img/a4282832058_10.jpg",
-            artist : "Justin Bieber",
-            audio : "https://media.hungama.com/c/4/ec0/a02/51733776/51733776_128.mp3?O8UuHF539mA2EZfv7PmR-MxuqW78azphDPD7-I6Wa_An3S25XXcuBGTK86P2imbVelx0f2lu-JafhNKwTLRMZmhYUkrlNWJmyBCjysmonqqfKDDq1wYkPirVsBLb23HC6zwGOA",
-            color : ["#CE3F33","#93282D"],
-            id : uuidv4(),
-            active : false
-        },
-        {
-            name : "Be kind",
-            cover : "https://www.lololyrics.com/img/cover/39824.jpeg",
-            artist : "Marshmello, Hasley",
-            audio : "https://media.hungama.com/c/4/2fb/1ce/58185306/58185306_128.mp3?8dG50JXafEYL02LsuFXohd4kBRi7NthkNdfkZH5-BCtQMAa7z10-GbJST8vaXs2dBwWjL4YAGORMJNPpvtx1KIsTmUcpnIVtvnTYqqbVQ90KqiCpjRWgVdersWzRGGvJgJ6KYQ",
-            color : ["#F1727B","#C7DEE2"],
-            id : uuidv4(),
-            active : false
-        },
-        {
-            name : "Wrong Direction",
-            cover : "https://m.media-amazon.com/images/I/81DgGIc6ijL._SS500_.jpg",
-            artist : "Hailee Steinfeld",
-            audio : "https://media.hungama.com/c/4/2a4/c5e/53505707/53505707_128.mp3?OKnljd7eOMVQTLlQ00peI2bwL-x1pFoOEE470Mxz6pdS1f3C205ECx6w5adrAT8Lg1KFh7JyVswV0ap-skqHInX8nmgA4G5FZF0oo9wDkO5bNdbg4e-HNdmdf2hwEAK0xl_BBw",
-            color : ["#C63E64","#FFDDEA"],
-            id : uuidv4(),
-            active : false
-        },
-        {
-            name : "Always i'll care",
-            cover : "https://images.genius.com/d38bc193eaa95ac28da1507867856d7b.1000x1000x1.jpg",
-            artist : "Jeremy Zucker",
-            audio : "https://media.hungama.com/c/4/59f/819/53142693/53142693_128.mp3?E8HiTqimAP-75fqvqRTqLr3NI7eYp_gONvWxsBL9nmhPO91JYEN5PtVv24o6LiEwfq26xYqcTgfCWYaiAwNcrAfP8vermYDJ11MrHuWCVH79MXxyGW3kWGeIHZXlLNwZofhNUA",
-            color : ["#4E5068","#CC6F56"],
-            id : uuidv4(),
-            active : false
-        },
-        {
-            name : "All you need to know",
-            cover : "https://images.hungama.com/c/1/e3b/ffe/61120983/61120983_300x300.jpg",
-            artist : "Gryffin",
-            audio : "https://media.hungama.com/c/4/63b/c48/61120843/61120843_128.mp3?2DWxMSIrXD_B2aqAzZdvHE71mkxf-qO5ZKV8B2Vb2BebeE8ufWagXIrRhpPYWKfgajqG1xgc5z2se1az0ggEIHjEUjXLq_hC2LnN96QnnkT2jhr_Wg3IzxXQ13Kb9tUI2B2mqQ",
-            color : ["#BCE5F9","#6E7F8F"],
-            id : uuidv4(),
-            active : false
-        },
-        {
             name : "Stacy",
             cover : "https://images.genius.com/353670ca2f209fa9670dd08cf4f06151.1000x1000x1.jpg",
             artist : "Quinn XCII",
             audio : "https://media.hungama.com/c/4/d7f/894/49025100/49025100_128.mp3?rgQ9Y4o6zqUZyRBDCFA1W4BfEEsyhBqI19SdtGBqc4ciKsRQpmIDeeUvK4I1leK9KZHbgFEwSFmHFnVuEkb_0Pfvb2dYCrA-1nAglS_YD7jIvr3o1-WetGVlopEkqo-urNSlCA",
             color : ["#63690E","#E9E8ED"],
-            id : uuidv4(),
-            active : false
-        },
-        {
-            name : "This is Us",
-            cover : "https://abokiplay.com/wp-content/uploads/2020/02/Tis-Is-us.jpg",
-            artist : "Jimmi allen, Noah Cyrus",
-            audio : "https://media.hungama.com/c/4/6f8/816/51955590/51955590_128.mp3?YX08vHhMHnYd_j6MC2h80HTNJoQrz14L0PQxwLhqdbP0SiqU_hgm0fqVl51_KwCmo3UVDVs4EL6e8nZXnM52OksXPJcDRv9tTpUCeQXTy97xeKnIhRkbxX_ElDheBbQbQHCVFg",
-            color : ["#545454","#E9E8ED"],
-            id : uuidv4(),
-            active : false
-        },
-        {
-            name : "What A Time",
-            cover : "https://www.rollingstone.com/wp-content/uploads/2019/02/julia_michaels-main_press_photo-_pc_clare_gillenW.jpg",
-            artist : "Julia Michaels",
-            audio : "https://media.hungama.com/c/4/6f5/358/46473866/46473866_128.mp3?cwAb8n38RCopD_FwYxDo9q7Y_Hqxv5ADGXHVuFcrq6cW9HefgU-0qM9d32AtzjIZMT2ABqnxRrtF5NJBUQTCoWbvjqzBnD8NoR-VrU9OFTd3zM-0Z6whP4JVlHNEous4vPmsMQ",
-            color : ["#C39884","#FFFFFF"],
-            id : uuidv4(),
-            active : false
-        },
-        {
-            name : "Slow",
-            cover : "https://images.hungama.com/c/1/262/45b/55760515/55760515_300x300.jpg",
-            artist : "Shy Martin",
-            audio : "https://media.hungama.com/c/4/f92/a34/54594370/54594370_128.mp3?d8GHbEokT-sUJefUggiNVcP3O75eDsAZQq1_Vzxn5u-yB8tpZqh-v09KsCdmVtBAgkYt8MU8o-EBLGx8SzEdfSa4WfmxryCXXjTL74_8aP4J2-vSaRSXyFuCKgkd1fFwjoVY5w",
-            color : ["#532C45","#B9605C"],
             id : uuidv4(),
             active : false
         },
@@ -116,6 +35,15 @@ function music() {
             artist : "Khalid",
             audio : "https://media.hungama.com/c/4/db1/198/47561033/47561033_128.mp3?Q5rkR0NiR5cwl--yau-m3tmxodmOzG7QB8PXQcC16UYsavOSfVK_52ybQMXdg4F03vgwNPKeiWm7R5ctqYF_QAR15poN2IBJaq3gNLhd24BeSo9HYaOk-VFA4FMr8K5AIXVNyA",
             color : ["#956222","#85B3CE"],
+            id : uuidv4(),
+            active : false
+        },
+        {
+            name : " Vintage",
+            cover : "https://chillhop.com/wp-content/uploads/2021/04/cb0cc6270d7f2e1bb13e44e8832bd5c9b2a61080-1024x1024.jpg",
+            artist : "Evil Needle",
+            audio : "https://mp3.chillhop.com/serve.php/?mp3=17088",
+            color : ["#E5AF4D","#A19082"],
             id : uuidv4(),
             active : false
         },
